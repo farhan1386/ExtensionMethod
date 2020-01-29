a981c63b498f435c04621831ac9e0fa70f225384
Farhan Ahmed <farhan1086@gmail.com>
2020-01-29 19:55:19 +0530

    initial commit

---
+++ ExtensionMethod/ExtentionClass.cs
@@ -0,0 +1,10 @@
+﻿namespace ExtensionMethod
+{
+    public static class ExtentionClass
+    {
+        public static double CalculateVat(this Product p)
+        {
+            return (p.Price * .25);
+        }
+    }
+}
--- ExtensionMethod/Program.cs
+++ ExtensionMethod/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExtensionMethod
 {
@@ -10,6 +7,24 @@ namespace ExtensionMethod
     {
         static void Main(string[] args)
         {
+            List<Product> product = new List<Product>()
+            {
+                new Product { Id = 1, ProductName = "Mobile Phone", Price = 10000 },
+                new Product { Id = 2, ProductName = "Headphone", Price = 550 },
+                new Product { Id = 3, ProductName = "Bluetooth", Price = 600 }
+            };
+
+            foreach (var item in product)
+            {
+                Console.WriteLine("Product Name:{0} \t Price:{1}", item.ProductName,item.Price);
+            }
+
+            Console.WriteLine("\n-----Product With VAT-----");
+            foreach (var item in product)
+            {
+                Console.WriteLine("Product Name:{0} Price:{1} VAT:{2}", item.ProductName, item.Price, item.CalculateVat());
+            }
+            Console.ReadLine();
         }
     }
 }
